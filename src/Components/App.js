8d27b6622d9ee7e0d7b670643bcc8153c70d4acb
Rohan Chugh <rohanchugh14@gmail.com>
2024-03-26 02:13:18 -0500

    maybe new stuff idk

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,6 +6,7 @@ import Simulator from "./Simulator";
 import Home from "./Home";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ReadyPlayerCreditRedirect from "./ReadyPlayerCreditRedirect";
 import Header from "./Header";
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/scraper" element={<Scraper />} />
         <Route path="/simulator" element={<Simulator />} />
+        <Route path="/ready_player_credit" element = {<ReadyPlayerCreditRedirect />} />
       </Routes>
     </Router>
       // <Simulator />
--- src/Components/Home/index.tsx
+++ src/Components/Home/index.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Intro from "./Intro";
 import "../../CSS/Home.scss";
+import { Card } from "react-bootstrap";
 const Home = () => {
   return (
     <>
+    <Card>
+      <Card.Body>
+        <Card.Title><span className="title">Hello!</span></Card.Title>
+        <Card.Text>
+          Welcome to my website!
+        </Card.Text>
+      </Card.Body>
+    </Card>
       <Intro />
     </>
   );
