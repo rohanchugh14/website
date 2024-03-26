da8eb2046f98db8825cd2af3d36e87db9cafd198
Rohan Chugh <rohanchugh14@gmail.com>
2024-03-26 02:24:18 -0500

    added redirect for ready player credit

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,12 +3,20 @@ import React from "react";
 // import Navbar from "./Navbar";
 import "../CSS/App.scss";
 import Scraper from "./Scraper";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import ReadyPlayerCreditRedirect from "./ReadyPlayerCreditRedirect";
 
 const App = () => {
   return (
     <div>
       {/* <Navbar /> */}
-        <Scraper />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Scraper />} />
+        <Route path="/ready_player_credit" element={<ReadyPlayerCreditRedirect/>} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+        </BrowserRouter>
     </div>
   )
 }
---
+++ src/Components/ReadyPlayerCreditRedirect/index.jsx
@@ -0,0 +1,11 @@
+import React, {useEffect} from 'react'
+
+const ReadyPlayerCreditRedirect = () => {
+  useEffect(() => {
+    window.location.href = 'https://readyplayercredit.netlify.app/';
+  }, []);
+
+  return null; // Render nothing or a loading indicator
+}
+
+export default ReadyPlayerCreditRedirect
